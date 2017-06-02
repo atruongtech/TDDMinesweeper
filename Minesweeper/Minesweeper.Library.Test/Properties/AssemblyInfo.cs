59ab6fe5b11f462dbf98baa915b39e1d305234d6
Alan <atruongcontact@gmail.com>
2017-06-01 22:49:40 -0700

    Creation of class libraries and projects

---
+++ Minesweeper/Minesweeper.Library.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Minesweeper.Library.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Minesweeper.Library.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("74b2195d-bacf-49c0-a2b3-1c9d87067101")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Minesweeper/Minesweeper.Library.Test/TileTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Minesweeper.Library.Test
+{
+    [TestClass]
+    public class TileTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
