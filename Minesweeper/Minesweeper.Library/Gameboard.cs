f29254775d08ed9bf34d63459abde3636a1302d4
Alan Truong <atruongcontact@gmail.com>
2018-04-16 16:40:37 -0700

    Add reveal all mines if game ended when user clicks a mine

--- Minesweeper/Minesweeper.Library/Gameboard.cs
+++ Minesweeper/Minesweeper.Library/Gameboard.cs
@@ -1,281 +1,293 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Prism.Mvvm;
 using Prism.Commands;
 using System.Timers;
 
 namespace Minesweeper.Library
 {
-    public class Gameboard : BindableBase
-    {
-        private int _rows;
-        private int _columns;
-        private int _numMines;
-        private List<Tile> _tiles;
-        private DifficultySetting _settings;
-        private bool _gameOver;
-        private bool _win;
-        private int _playTime;
-        private Timer _timer;
+   public class Gameboard : BindableBase
+   {
+      private int _rows;
+      private int _columns;
+      private int _numMines;
+      private List<Tile> _tiles;
+      private DifficultySetting _settings;
+      private bool _gameOver;
+      private bool _win;
+      private int _playTime;
+      private Timer _timer;
 
-        private int _tilesLeft;
+      private int _tilesLeft;
 
-        #region properties
-        public int Rows
-        {
-            get { return _rows; }
-            set { SetProperty(ref _rows, value); }
-        }
+      #region properties
+      public int Rows
+      {
+         get { return _rows; }
+         set { SetProperty(ref _rows, value); }
+      }
 
-        public int Columns
-        {
-            get { return _columns; }
-            set { SetProperty(ref _columns, value); }
-        }
+      public int Columns
+      {
+         get { return _columns; }
+         set { SetProperty(ref _columns, value); }
+      }
 
-        public List<Tile> Tiles
-        {
-            get { return _tiles; }
-            set { SetProperty(ref this._tiles, value); }
-        }
+      public List<Tile> Tiles
+      {
+         get { return _tiles; }
+         set { SetProperty(ref this._tiles, value); }
+      }
 
-        public int NumMines
-        {
-            get { return _numMines; }
-            set { SetProperty(ref this._numMines, value); }
-        }
+      public int NumMines
+      {
+         get { return _numMines; }
+         set { SetProperty(ref this._numMines, value); }
+      }
 
-        public DifficultySetting Settings
-        {
-            get { return _settings; }
-            set { SetProperty(ref this._settings, value); }
-        }
+      public DifficultySetting Settings
+      {
+         get { return _settings; }
+         set { SetProperty(ref this._settings, value); }
+      }
 
-        public bool GameOver
-        {
-            get { return _gameOver; }
-            set { SetProperty(ref this._gameOver, value); }
-        }
+      public bool GameOver
+      {
+         get { return _gameOver; }
+         set { SetProperty(ref this._gameOver, value); }
+      }
 
-        public bool Win
-        {
-            get { return _win; }
-            set { SetProperty(ref this._win, value); }
-        }
+      public bool Win
+      {
+         get { return _win; }
+         set { SetProperty(ref this._win, value); }
+      }
 
-        public int PlayTime
-        {
-            get { return _playTime; }
-            set { SetProperty(ref this._playTime, value); }
-        }
+      public int PlayTime
+      {
+         get { return _playTime; }
+         set { SetProperty(ref this._playTime, value); }
+      }
 
-        public DelegateCommand<Tile> RevealCommand
-        {
-            get;
-            private set;
-        }
-        
-        public DelegateCommand<Tile> ToggleTileMarkedCommand
-        {
-            get;
-            private set;
-        }
+      public DelegateCommand<Tile> RevealCommand
+      {
+         get;
+         private set;
+      }
 
-        
+      public DelegateCommand<Tile> ToggleTileMarkedCommand
+      {
+         get;
+         private set;
+      }
 
-        #endregion
 
-        public Gameboard()
-        {
-            this.RevealCommand = new DelegateCommand<Tile>(RevealTiles);
-            this.ToggleTileMarkedCommand = new DelegateCommand<Tile>(ToggleTileMarked);
 
-            this.GameOver = false;
-        }
+      #endregion
 
-        public Gameboard(DifficultyLevel level) : this()
-        {
-            this.Settings = new DifficultySetting(level);
+      public Gameboard()
+      {
+         this.RevealCommand = new DelegateCommand<Tile>(RevealTiles);
+         this.ToggleTileMarkedCommand = new DelegateCommand<Tile>(ToggleTileMarked);
 
-            Tiles = new List<Tile>();
-            SetDimensions(this.Settings.Rows, this.Settings.Columns);
-            PopulateTiles();
-            SeedMines();
-            SetNeighborMineCounts(this.Tiles, this.Columns);
-            StartPlayTimer();
-        }
+         this.GameOver = false;
+      }
 
-        public static void SetNeighborMineCounts(List<Tile> tiles, int columns)
-        {
-            List<Tile> neighbors;
+      public Gameboard(DifficultyLevel level) : this()
+      {
+         this.Settings = new DifficultySetting(level);
 
-            foreach(Tile tile in tiles)
-            {
-                if (tile.IsMine)
-                {
-                    neighbors = Gameboard.GetAllNeighbors(tile.TileIndex, tiles, columns);
-                    foreach (Tile neighbor in neighbors)
-                        neighbor.NumNeighborMines = neighbor.NumNeighborMines + 1;
-                }
-            }
-        }
+         Tiles = new List<Tile>();
+         SetDimensions(this.Settings.Rows, this.Settings.Columns);
+         PopulateTiles();
+         SeedMines();
+         SetNeighborMineCounts(this.Tiles, this.Columns);
+         StartPlayTimer();
+      }
 
-        public static List<Tile> GetAllNeighbors(int index, List<Tile> tiles, int columns)
-        {
-            List<Tile> neighbors = new List<Tile>();
+      public static void SetNeighborMineCounts(List<Tile> tiles, int columns)
+      {
+         List<Tile> neighbors;
 
-            // top, middle, bottom
-            // left, middle, right
-            int tl = index - columns - 1;
-            int tm = index - columns;
-            int tr = index - columns + 1;
-
-            int ml = index - 1;
-            int mr = index + 1;
-
-            int bl = index + columns - 1;
-            int bm = index + columns;
-            int br = index + columns + 1;
-
-            // index is not the first column
-            // and neighbor is a valid index
-            if (index % columns > 0 && tl >= 0)
-                neighbors.Add(tiles[tl]);
-
-            if (tm >= 0)
-                neighbors.Add(tiles[tm]);
-
-            // index is not in last column
-            // and neighbor is a valid index
-            if (index % columns < (columns - 1) && tr >= 0)
-                neighbors.Add(tiles[tr]);
-
-            if (index % columns > 0)
-                neighbors.Add(tiles[ml]);
-
-            if (index % columns < (columns - 1))
-                neighbors.Add(tiles[mr]);
-
-            if (index % columns > 0 && bl < (tiles.Count))
-                neighbors.Add(tiles[bl]);
-
-            if (bm < tiles.Count)
-                neighbors.Add(tiles[bm]);
-
-            if (index % columns < (columns - 1) && br < (tiles.Count))
-                neighbors.Add(tiles[br]);
-
-            return neighbors;
-
-        }
-
-        public void RevealTiles(Tile tile)
-        {
-            if (tile.IsMarked)
-                return;
-
-            tile.Reveal();
-            this._tilesLeft--;
-
+         foreach (Tile tile in tiles)
+         {
             if (tile.IsMine)
             {
-                EndGame(false);
-                return;
+               neighbors = Gameboard.GetAllNeighbors(tile.TileIndex, tiles, columns);
+               foreach (Tile neighbor in neighbors)
+                  neighbor.NumNeighborMines = neighbor.NumNeighborMines + 1;
             }
+         }
+      }
 
-            if (tile.NumNeighborMines == 0 && !tile.IsMine)
+      public static List<Tile> GetAllNeighbors(int index, List<Tile> tiles, int columns)
+      {
+         List<Tile> neighbors = new List<Tile>();
+
+         // top, middle, bottom
+         // left, middle, right
+         int tl = index - columns - 1;
+         int tm = index - columns;
+         int tr = index - columns + 1;
+
+         int ml = index - 1;
+         int mr = index + 1;
+
+         int bl = index + columns - 1;
+         int bm = index + columns;
+         int br = index + columns + 1;
+
+         // index is not the first column
+         // and neighbor is a valid index
+         if (index % columns > 0 && tl >= 0)
+            neighbors.Add(tiles[tl]);
+
+         if (tm >= 0)
+            neighbors.Add(tiles[tm]);
+
+         // index is not in last column
+         // and neighbor is a valid index
+         if (index % columns < (columns - 1) && tr >= 0)
+            neighbors.Add(tiles[tr]);
+
+         if (index % columns > 0)
+            neighbors.Add(tiles[ml]);
+
+         if (index % columns < (columns - 1))
+            neighbors.Add(tiles[mr]);
+
+         if (index % columns > 0 && bl < (tiles.Count))
+            neighbors.Add(tiles[bl]);
+
+         if (bm < tiles.Count)
+            neighbors.Add(tiles[bm]);
+
+         if (index % columns < (columns - 1) && br < (tiles.Count))
+            neighbors.Add(tiles[br]);
+
+         return neighbors;
+
+      }
+
+      public void RevealTiles(Tile tile)
+      {
+         if (tile.IsMarked)
+            return;
+
+         tile.Reveal();
+         this._tilesLeft--;
+
+         if (tile.IsMine)
+         {
+            EndGame(false);
+            return;
+         }
+
+         if (tile.NumNeighborMines == 0 && !tile.IsMine)
+         {
+            Queue<Tile> checkQueue = new Queue<Tile>();
+            checkQueue.Enqueue(tile);
+            while (checkQueue.Count > 0)
             {
-                Queue<Tile> checkQueue = new Queue<Tile>();
-                checkQueue.Enqueue(tile);
-                while (checkQueue.Count > 0)
-                {
-                    Tile neighbor = checkQueue.Dequeue();
+               Tile neighbor = checkQueue.Dequeue();
 
-                    if (!neighbor.IsRevealed)
-                    {
-                        neighbor.Reveal();
-                        this._tilesLeft--;
-                    }                    
+               if (!neighbor.IsRevealed)
+               {
+                  neighbor.Reveal();
+                  this._tilesLeft--;
+               }
 
-                    if (neighbor.NumNeighborMines == 0)
-                    {
-                        foreach(Tile newNeighbor in Gameboard.GetAllNeighbors(neighbor.TileIndex, this.Tiles, this.Columns))
-                        {
-                            if (!newNeighbor.IsRevealed && !checkQueue.Contains(newNeighbor))
-                                checkQueue.Enqueue(newNeighbor);
-                        }
-                    }
-                }
+               if (neighbor.NumNeighborMines == 0)
+               {
+                  foreach (Tile newNeighbor in Gameboard.GetAllNeighbors(neighbor.TileIndex, this.Tiles, this.Columns))
+                  {
+                     if (!newNeighbor.IsRevealed && !checkQueue.Contains(newNeighbor))
+                        checkQueue.Enqueue(newNeighbor);
+                  }
+               }
             }
+         }
 
-            if (this._tilesLeft == this.Settings.Mines)
-                EndGame(true);
-        }
+         if (this._tilesLeft == this.Settings.Mines)
+            EndGame(true);
+      }
 
-        public void ToggleTileMarked(Tile tile)
-        {
-            if (tile.IsMarked)
-                this.NumMines++;
-            else if (this.NumMines < 1)
-                return;
-            else
-                this.NumMines--;
+      public void ToggleTileMarked(Tile tile)
+      {
+         if (tile.IsMarked)
+            this.NumMines++;
+         else if (this.NumMines < 1)
+            return;
+         else
+            this.NumMines--;
 
-            tile.ToggleMark();
-        }
+         tile.ToggleMark();
+      }
 
-        private void PopulateTiles()
-        {
-            for (int i = 0; i < this.Rows * this.Columns; i ++)
+      private void PopulateTiles()
+      {
+         for (int i = 0; i < this.Rows * this.Columns; i++)
+         {
+            this.Tiles.Add(new Tile(i));
+         }
+         this._tilesLeft = this.Rows * this.Columns;
+      }
+
+      private void SeedMines()
+      {
+         Random rng = new Random();
+         this.NumMines = 0;
+
+         while (this.NumMines < this.Settings.Mines)
+         {
+            int position = rng.Next(0, (this.Rows * this.Columns));
+            if (!this.Tiles[position].IsMine)
             {
-                this.Tiles.Add(new Tile(i));
+               this.Tiles[position].IsMine = true;
+               this.NumMines++;
             }
-            this._tilesLeft = this.Rows * this.Columns;
-        }
+         }
+      }
 
-        private void SeedMines()
-        {
-            Random rng = new Random();
-            this.NumMines = 0;
+      private void SetDimensions(int rows, int columns)
+      {
+         this.Rows = rows;
+         this.Columns = columns;
+      }
 
-            while (this.NumMines < this.Settings.Mines)
-            {
-                int position = rng.Next(0, (this.Rows * this.Columns));
-                if (!this.Tiles[position].IsMine)
-                {
-                    this.Tiles[position].IsMine = true;
-                    this.NumMines++;
-                }
-            }            
-        }
+      private void StartPlayTimer()
+      {
+         this._timer = new Timer(1000);
+         this._timer.Elapsed += ((s, e) => { this.PlayTime++; });
+         this._timer.Enabled = true;
+         this._timer.Start();
+      }
 
-        private void SetDimensions(int rows, int columns)
-        {
-            this.Rows = rows;
-            this.Columns = columns;
-        }
+      private void StopPlayTimer()
+      {
+         this._timer.Stop();
+         this._timer.Dispose();
+      }
 
-        private void StartPlayTimer()
-        {
-            this._timer = new Timer(1000);
-            this._timer.Elapsed += ((s, e) => { this.PlayTime++; });
-            this._timer.Enabled = true;
-            this._timer.Start();
-        }
+      private void EndGame(bool win)
+      {
+         StopPlayTimer();
+         if (win)
+            this.Win = true;
+         else
+         {
+            RevealAllMines();
+            this.GameOver = true;
+         }
+      }
 
-        private void StopPlayTimer()
-        {
-            this._timer.Stop();
-            this._timer.Dispose();
-        }
-
-        private void EndGame(bool win)
-        {
-            StopPlayTimer();
-            if (win)
-                this.Win = true;
-            else
-                this.GameOver = true;
-        }
-    }
+      private void RevealAllMines()
+      {
+         foreach (var tile in _tiles.Where(t => t.IsMine))
+         {
+            tile.Reveal();
+         }
+      }
+   }
 }
